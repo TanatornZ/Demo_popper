5ed33d76c5dd10892cf514daf67961a6456dd2f0
TanatornZ <80368758+TanatornZ@users.noreply.github.com>
2022-05-23 17:46:08 +0700

    add flip popper

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { usePopper } from "react-popper";
 import "./App.css";
 import Select from "react-select";
 import { Placement } from "@popperjs/core";
+import Filp from "./components/Filp";
 
 function App() {
   const [option, setOption] = useState<string>("");
   const [position, setPosition] = useState<Placement>("top");
+  const [showPopper, setShowPopper] = useState<boolean>(false);
 
   const options = [
     { value: "top", label: "top" },
@@ -15,31 +17,12 @@ function App() {
     { value: "bottom", label: "bottom" },
   ];
 
-  useEffect(() => {
-    switch (option) {
-      case "left":
-        setPosition("left");
-        break;
-      case "right":
-        setPosition("right");
-        break;
-      case "top":
-        setPosition("top");
-        break;
-      case "bottom":
-        setPosition("bottom");
-        break;
-    }
-  }, [option]);
-
   const [referenceElement, setReferenceElement] =
     useState<HTMLButtonElement | null>(null);
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
     null
   );
   const [arrowElement, setArrowElement] = useState<HTMLDivElement | null>(null);
-
-  const [showPopper, setShowPopper] = useState<boolean>(false);
 
   //popper
   const { styles, attributes } = usePopper(referenceElement, popperElement, {
@@ -55,36 +38,77 @@ function App() {
     ],
   });
 
+  const showToolTip = () => {
+    popperElement?.setAttribute("data-show", "");
+  };
+
+  const hideToolTip = () => {
+    popperElement?.removeAttribute("data-show");
+  };
+  const showEvents = ["mouseenter", "focus"];
+  const hideEvents = ["mouseleave", "blur"];
+
+  showEvents.forEach((event) => {
+    referenceElement?.addEventListener(event, showToolTip);
+  });
+
+  hideEvents.forEach((event) => {
+    referenceElement?.addEventListener(event, hideToolTip);
+  });
+
   return (
-    <div className="">
-      <div className="container w-screen h-screen bg-slate-500 flex flex-col">
-        <div className="w-36 h-36 mx-auto mt-12">
-          <Select
-            onChange={(e) => {
-              const option = e?.value;
-              if (option) setOption(option);
-            }}
-            defaultValue={options[0]}
-            options={options}
-          />
-        </div>
-        <button
-          className="p-5 rounded-lg bg-white self-center"
-          ref={(ref) => setReferenceElement(ref)}
-          onClick={() => setShowPopper(!showPopper)}
-        >
-          click to show popper
-        </button>
-        <p
-          className={`p-2 rounded-lg bg-green-400 ${!showPopper && "hidden"}`}
-          ref={(ref) => setPopperElement(ref)}
-          style={styles.popper}
-          {...attributes.popper}
-        >
-          popper
-          <div id='arrow' ref={(ref) => setArrowElement(ref)} style={styles.arrow}  />
-        </p>
+    <div className=" w-screen h-screen bg-slate-500 flex flex-col">
+      <div className="w-36 h-36 mx-auto mt-12">
+        <Select
+          onChange={(e) => {
+            const option = e?.value;
+            if (option) {
+              setOption(option);
+              switch (option) {
+                case "left":
+                  setPosition("left");
+                  break;
+                case "right":
+                  setPosition("right");
+                  break;
+                case "top":
+                  setPosition("top");
+                  break;
+                case "bottom":
+                  setPosition("bottom");
+                  break;
+              }
+            }
+          }}
+          defaultValue={options[0]}
+          options={options}
+        />
       </div>
+      <button
+        className="p-5 rounded-lg bg-red-100 self-center"
+        ref={(ref) => setReferenceElement(ref)}
+        id="button"
+        aria-describedby="tooltip"
+        // onClick={() => setShowPopper(!showPopper)}
+      >
+        click to show popper
+      </button>
+      <p
+        id="tooltip"
+        ref={(ref) => setPopperElement(ref)}
+        style={styles.popper}
+        {...attributes.popper}
+      >
+        popper
+        <div
+          id="arrow"
+          ref={(ref) => setArrowElement(ref)}
+          style={styles.arrow}
+          data-popper-arrow
+        ></div>
+      </p>
+
+      <Filp />
     </div>
   );
 }
---
+++ src/components/Filp.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { usePopper } from "react-popper";
+
+type Props = {};
+
+function Filp({}: Props) {
+  const [referenceFlipElement, setReferenceFlipElement] =
+    useState<HTMLDivElement | null>(null);
+
+  const [flipToolTip, setFlipToolTip] = useState<HTMLDivElement | null>(null);
+  const [arrowFlipElement, setArrowFlipElement] =
+    useState<HTMLDivElement | null>(null);
+
+  const { styles, attributes } = usePopper(referenceFlipElement, flipToolTip, {
+    placement: "auto",
+    modifiers: [
+      {
+        name: "flip",
+        options: {
+          allowedAutoPlacements: ["top", "bottom", "right", "left"],
+        },
+      },
+      {
+        name: "offset",
+        options: {
+          offset: [0, 8],
+        },
+      },
+      { name: "arrow", options: { element: arrowFlipElement } },
+    ],
+  });
+
+  return (
+    <div className="w-96 h-96 rounded-lg border-4 border-dotted overflow-auto  bg-white mx-auto mt-24 ">
+      <div className="w-[800px] h-[800px] bg-slate-300 flex justify-center items-center">
+        <div id="container">
+          <div
+            id="popcorn"
+            aria-describedby="flipTooltip"
+            ref={(ref) => setReferenceFlipElement(ref)}
+          ></div>
+          <div
+            id="flipTooltip"
+            ref={(ref) => setFlipToolTip(ref)}
+            role="tooltip"
+            style={styles.popper}
+            {...attributes.popper}
+          >
+            <div>Popcorn</div>
+            <div>New Item</div>
+            <div
+              id="arrow"
+              data-popper-arrow
+              style={styles.arrow}
+              ref={(ref) => setArrowFlipElement(ref)}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Filp;
